a3df1b6dee534aaae62880e49d6fe1c72c573c13
Pieter Rossouw <prossouw79@gmail.com>
2019-09-21 05:36:43 +0200

    Add nano to container

--- nats-bench/client.sh
+++ nats-bench/client.sh
@@ -2,5 +2,5 @@
 
 ARCH=$(uname -m)
 docker build -t pietersynthesis/nats-bench:$ARCH .
-
+docker containter rm nats-bench
 docker run --rm -it --network host --name nats-bench --mount src="$(pwd)/results",target=/opt/results,type=bind pietersynthesis/nats-bench:$ARCH bash
